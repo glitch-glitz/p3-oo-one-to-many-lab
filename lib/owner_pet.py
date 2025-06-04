64f4fad12955f3054d750c7914cf53cbf6e09003
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-06-04 18:42:53 +0300

    Done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,50 @@
+# class Pet:
+#     pass
+
+# class Owner:
+#     pass
+
+
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.name = name
+        self.pet_type = pet_type
+        self._owner = None
+        if owner:
+            self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, new_owner):
+        from .owner import Owner  # avoid circular import at top level
+
+        if not isinstance(new_owner, Owner):
+            raise Exception("owner must be an instance of Owner")
+        self._owner = new_owner
+
+# from .pet import Pet
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Must be an instance of Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
